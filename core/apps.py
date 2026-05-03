b7de35732c771c299b7d9653029e99095f358348
Melika Tavakoli <122203275+melikatavakoli@users.noreply.github.com>
2026-05-03 17:39:03 +0330

    Create apps.py

---
+++ core/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "core"
+    verbose_name = "core"
+
+    def ready(self):
+        from users import signals
