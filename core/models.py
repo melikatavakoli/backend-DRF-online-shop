4cb196e775ee6fc0a85bde1216a33db6894c8e61
Melika Tavakoli <122203275+melikatavakoli@users.noreply.github.com>
2026-05-03 17:40:46 +0330

    Update models.py

--- core/models.py
+++ core/models.py
@@ -7,7 +7,33 @@ from common.models import BaseModel
 from users.managers import UserManager
 from users.types import RoleType, StatusType
 
+from django.contrib.auth.models import BaseUserManager
 
+from common.models import SoftDeleteManager
+
+
+class UserManager(BaseUserManager, SoftDeleteManager):
+    use_in_migrations = True
+
+    def _create_user(self, mobile, password, **extra_fields):
+        if not mobile:
+            raise ValueError("Mobile number is required")
+        
+        user = self.model(mobile=mobile, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, mobile, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(mobile, password, **extra_fields)
+
+    def create_superuser(self, mobile, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self._create_user(mobile, password, **extra_fields)
+        
 class BaseUser(AbstractUser, BaseModel):
     username = None
     mobile = models.CharField(max_length=11, unique=True)
