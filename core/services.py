458c6ed611cd59ad91e25f425566e72eb7aa29fd
Melika Tavakoli <122203275+melikatavakoli@users.noreply.github.com>
2026-05-03 17:52:18 +0330

    Create services.py

---
+++ core/services.py
@@ -0,0 +1,21 @@
+from users.models import LoginAttempt
+from users.types import StatusType
+
+
+def _get_ip(request) -> str | None:
+    if not request:
+        return None
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+    return (
+        x_forwarded_for.split(",")[0]
+        if x_forwarded_for
+        else request.META.get("REMOTE_ADDR")
+    )
+
+
+def record_login_attempt(request, user=None, status=StatusType.FAILED):
+    LoginAttempt.objects.create(
+        user=user,
+        status=status,
+        ip_address=_get_ip(request),
+    )
